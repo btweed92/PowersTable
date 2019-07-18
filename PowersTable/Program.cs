67667067a59b48f7cfbe7592a3f222b627292246
Brandyn Tweedly <tweedly92@gmail.com>
2019-07-18 18:04:54 -0400

    FirstCommit

---
+++ PowersTable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PowersTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, it is time to LEARN SQUARES AND CUBES!");
+
+            bool learnMore = true;
+            while (learnMore)
+            {
+                Console.WriteLine("Would you like to do squares and cubes? (y/n)");
+
+                if (Console.ReadLine() == "y")
+                {
+                    Display(ParseInput());
+                }
+                else
+                {
+                    learnMore = false;
+                    Console.WriteLine("Too bad, have a good day.");
+                }
+            }
+        }
+        public static void Display(int num)
+        {
+            Console.WriteLine("Number\tSquare\tCube");
+            Console.WriteLine("======\t======\t======");
+            for(int i=1; i <= num; i++)
+            {
+                Console.WriteLine($"{i}\t{SquarePower(i)}\t{CubePower(i)}");
+            }
+        }
+        public static int ParseInput()
+        {
+            Console.WriteLine("Please enter an integer");
+            return int.Parse(Console.ReadLine());
+        }
+        public static int SquarePower(int num)
+        {
+            return num *= num;
+        }
+        public static int CubePower(int num)
+        {
+            return num *= num *= num;
+        }
+    } 
+}
